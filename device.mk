18db7b36b5d6bd21b93d0601304934e74e0bf75c
=JacobDeepu <d2jdeepu@gmail.com>
2020-04-13 23:52:48 +0700

    land: Update QMI/Radio/IMS
    - From Nokia/Panther_00WW/PAN_sprout:10/QKQ1.191008.001/00WW_2_290:user/release-keys

--- device.mk
+++ device.mk
@@ -209,6 +209,7 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
     ims-ext-common \
     ims_ext_common.xml
 
@@ -319,8 +320,11 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libcnefeatureconfig \
